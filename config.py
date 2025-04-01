17ae0ccd8ca32a78c972a6cfb4f746f28442ca9b
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-01 18:01:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 LOG_CHANNEL = os.getenv("LOG_CHANNEL")
 FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
-PORT = int(os.getenv("PORT", 8080))
+
