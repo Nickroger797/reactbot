2d7aed38eb3d2831168496d9e46b8f0eeb808505
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-01 11:06:50 +0530

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,31 @@
+start_message = """
+Welcome to the Reaction Bot! 🤖
+Here are the commands you can use:
+1. /help - Get a list of all commands.
+2. /about - Information about the bot.
+3. /connect - To connect this bot to a group.
+4. /reaction - Use reaction commands.
+Enjoy!
+"""
+
+help_message = """
+Here are the commands for this bot:
+1. /start - Start the bot.
+2. /about - Get information about the bot.
+3. /help - Show help information.
+4. /connect [group_id] - Connect this bot to your group.
+5. /reaction - Use reaction features.
+"""
+
+about_message = """
+This bot is designed to add advanced reaction features to your Telegram group or chats.
+
+Features:
+1. Reaction Game Mode
+2. AI Mood-based Reactions
+3. Custom Stickers & GIF Reactions
+4. Connect to Groups with /connect
+5. And more!
+
+Developed by [Your Name]
+"""
