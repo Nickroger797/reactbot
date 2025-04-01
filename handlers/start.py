92147fbf153afde327e3f8141520506adfe3d66a
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-01 11:08:37 +0530

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from messages import start_message
+
+@Client.on_message()
+async def start_handler(client, message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    # Store user data
+    store_new_user(user_id, username)
+    await message.reply(start_message)
