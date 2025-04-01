6fc34140db6741f401580fe018b0f9316a8e5b0b
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-01 11:06:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Add environment variables or use a config file to manage sensitive data like tokens
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
+PORT = int(os.getenv("PORT", 8080))
