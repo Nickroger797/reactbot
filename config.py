91a8548d10ecc6aa8d5ec39e0f80505cff0f7ea6
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-03 08:13:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ import os
 API_ID = int(os.getenv("API_ID", 0))  # Default: 0 (अगर env var ना मिले)
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))  # Default: 0
 FORCE_SUB_CHANNEL = int(os.getenv("FORCE_SUB_CHANNEL", 0))  # Default: 0
 
 # Validate required variables
-if not all([API_ID, API_HASH, BOT_TOKEN]):
+if not all([API_ID, API_HASH, BOT_TOKEN, MONGO_URI]):
     raise ValueError("⚠️ Error: Required environment variables are missing!")
