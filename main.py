7733ed7a850fe692c12989cd636897f2eea7f7f8
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-03 23:02:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from pyrogram import Client
 from handlers import start, reaction_game, ai_reactions, connect, force_sub
-from config import BOT_TOKEN, LOG_CHANNEL, FORCE_SUB_CHANNEL, API_ID, API_HASH
+from config import BOT_TOKEN, LOG_CHANNEL, FORCE_SUB_CHANNEL, API_ID, API_HASH, MONGO_URI
 from db import store_new_user  
 import server  # Import server.py for fake web server (Koyeb health check)
 
