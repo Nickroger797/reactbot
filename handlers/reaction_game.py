d639d1a715d528d101e36a04fb8d9f74f492b486
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-04 12:33:12 +0530

    Update reaction_game.py

--- handlers/reaction_game.py
+++ handlers/reaction_game.py
@@ -1,7 +1,5 @@
-from pyrogram import Client
 from reactions import reaction_game
 
-@Client.on_message()
 async def reaction_game_handler(client, message):
     if message.text.startswith("/reaction"):
         reaction = reaction_game()
